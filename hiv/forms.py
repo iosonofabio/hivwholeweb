572649fd277f70b01af91f1f397a02403383488e
Fabio Zanini <fabio.zanini@tuebingen.mpg.de>
2014-10-01 09:29:11 +0200

    Test with d3.js

--- hiv/forms.py
+++ hiv/forms.py
@@ -19,3 +19,20 @@ class TreeForm(Form):
         else:
             return False
 
+
+class CoverageForm(Form):
+    F1 = BooleanField('F1', default=True)
+    F2 = BooleanField('F3')
+    F3 = BooleanField('F3')
+    F4 = BooleanField('F4')
+    F5 = BooleanField('F5')
+    F6 = BooleanField('F6')
+
+
+    def validate(self):
+        if super(CoverageForm, self).validate():
+            return any((self.F1.data, self.F2.data, self.F3.data,
+                        self.F4.data, self.F5.data, self.F6.data))
+        else:
+            return False
+
--- hiv/views.py
+++ hiv/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, request
 from hiv import hiv
-from forms import TreeForm
+from forms import TreeForm, CoverageForm
 from .backbone import sections
 
 
@@ -10,6 +10,7 @@ def index():
     return render_template('index.html',
                            title='Home',
                            sections=sections,
+                           section_name='Home',
                           )
 
 
@@ -18,6 +19,7 @@ def test():
     return render_template('test.html',
                            title='BokehJS test',
                            sections=sections,
+                           section_name='Test Bokeh',
                           )
 
 
@@ -39,26 +41,44 @@ def trees():
                            trees=trees,
                            form=form,
                            sections=sections,
+                           section_name='Phylogenetic trees',
                           )
 
 
-@hiv.route('/coverage/')
+@hiv.route('/coverage/', methods=['GET', 'POST'])
 def coverage():
-    fragment = 'F1'
+    form = CoverageForm()
+    if request.method == 'GET':
+        fragments = ['F1']
+    else:
+        fragments = ['F'+str(i+1) for i in xrange(6) if getattr(form, 'F'+str(i+1)).data]
+        if not form.validate_on_submit():
+            flash('Select at least one fragment!')
 
-    bokeh_dict = {'has_data': False}
     from test_bokeh import coverage
-    try:
-        (js, div) = coverage(fragment=fragment)
-        bokeh_dict['js'] = js
-        bokeh_dict['div'] = div
-        bokeh_dict['has_data'] = True
-    except IOError:
-        flash('Could not reach the source data.')
-
+    bokeh_dicts = []
+    for fragment in fragments:
+        bokeh_dict = {'has_data': False}
+        try:
+            (js, div) = coverage(fragment=fragment)
+            bokeh_dict['js'] = js
+            bokeh_dict['div'] = div
+            bokeh_dict['has_data'] = True
+        except IOError:
+            flash('Could not reach the source data for fragment '+fragment+'.')
+        bokeh_dicts.append(bokeh_dict)
 
     return render_template('coverage.html',
                            title='Coverage',
-                           bokeh_dict=bokeh_dict,
+                           bokeh_dicts=bokeh_dicts,
                            sections=sections,
+                           section_name='Coverage',
+                           form=form,
                           )
+
+
+@ hiv.route('/test_csv.csv')
+def test_csv():
+    with open('hiv/test_csv.csv', 'r') as f:
+        text = f.read()
+    return text
