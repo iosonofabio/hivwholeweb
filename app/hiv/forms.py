bdd2bb2dd0c67d953b4f5970b1aa712dd608538f
Fabio Zanini <fabio.zanini@tuebingen.mpg.de>
2014-12-12 12:53:04 +0100

    contact right aligned and refactored forms

--- app/hiv/forms.py
+++ app/hiv/forms.py
@@ -1,64 +1,22 @@
+# vim: fdm=indent
+'''
+author:     Fabio Zanini
+date:       12/12/14
+content:    Forms for hivwholeweb.
+'''
+# Modules
 from flask.ext.wtf import Form
 from wtforms import BooleanField, IntegerField, SelectField, FormField
 from wtforms.validators import Required
 
 
-class PatFragForm(Form):
-    F1 = BooleanField('F1', default=True)
-    F2 = BooleanField('F3')
-    F3 = BooleanField('F3')
-    F4 = BooleanField('F4')
-    F5 = BooleanField('F5')
-    F6 = BooleanField('F6')
 
-    p1 = BooleanField('p1', default=True)
-    p2 = BooleanField('p3')
-    p3 = BooleanField('p3')
-    p4 = BooleanField('p4')
-    p5 = BooleanField('p5')
-    p6 = BooleanField('p6')
-    p7 = BooleanField('p7')
-    p8 = BooleanField('p8')
-    p9 = BooleanField('p9')
-    p10 = BooleanField('p10')
-    p11 = BooleanField('p11')
-
-    def validate(self):
-        if super(PatFragForm, self).validate():
-            has_frag = any((self.F1.data, self.F2.data, self.F3.data,
-                            self.F4.data, self.F5.data, self.F6.data))
-            has_pat = any((self.p1.data, self.p2.data, self.p3.data,
-                           self.p4.data, self.p5.data, self.p6.data,
-                           self.p7.data, self.p8.data, self.p9.data,
-                           self.p10.data, self.p11.data))
-            return has_frag and has_pat
-        else:
-            return False
+# Globals
+_regions = ('V3', 'PR', 'psi')
 
 
-class PatForm(Form):
-    p1 = BooleanField('p1', default=True)
-    p2 = BooleanField('p3')
-    p3 = BooleanField('p3')
-    p4 = BooleanField('p4')
-    p5 = BooleanField('p5')
-    p6 = BooleanField('p6')
-    p7 = BooleanField('p7')
-    p8 = BooleanField('p8')
-    p9 = BooleanField('p9')
-    p10 = BooleanField('p10')
-    p11 = BooleanField('p11')
 
-    def validate(self):
-        if super(PatForm, self).validate():
-            return any((self.p1.data, self.p2.data, self.p3.data,
-                        self.p4.data, self.p5.data, self.p6.data,
-                        self.p7.data, self.p8.data, self.p9.data,
-                        self.p10.data, self.p11.data))
-        else:
-            return False
-
-
+# Classes
 class RoiForm(Form):
     # FIXME: allow all fragments, including HXB2 and genomewide
     region = SelectField('Region',
@@ -96,18 +54,15 @@ class PrecompiledHaplotypeForm(Form):
     patient = SelectField('Patient',
                           choices=[['p'+str(i)] * 2 for i in xrange(1, 12)])
     region = SelectField('Region',
-                         choices=[('V3', 'V3'),
-                                  ('PR', 'PR'),
-                                  ('psi', 'psi')])
+                         choices=[[reg] *2 for reg in _regions])
 
 
 class TreeForm(Form):
-    _regions = ('V3', 'PR', 'psi')
     patient = SelectField('Patient',
                           choices=[['all'] * 2] + [['p'+str(i)] * 2 for i in xrange(1, 12)])
     region = SelectField('Region',
                           choices=([['F'+str(i)] * 2 for i in xrange(1, 7)] +
-                                   [[reg] *2 for reg in _regions] +
+                                   [[reg] * 2 for reg in _regions] +
                                    [[reg+'minor', reg+' minor'] for reg in _regions]),
                         )
 
@@ -125,7 +80,6 @@ class PatFragSingleForm(Form):
 
 
 class ConsensiForm(Form):
-    _regions = ('V3', 'PR', 'psi')
     patient = SelectField('Patient',
                           choices=[['p'+str(i)] * 2 for i in xrange(1, 12)])
     fragment = SelectField('Fragment',
