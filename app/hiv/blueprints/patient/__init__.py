24f99654ff8884bf84e3eb9af9ff5455e9014546
Fabio Zanini <fabio.zanini@tuebingen.mpg.de>
2015-01-19 17:58:24 +0100

    Patient links

--- app/hiv/blueprints/patient/__init__.py
+++ app/hiv/blueprints/patient/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort
 
 
 patient = Blueprint('patient', __name__,
@@ -7,7 +7,13 @@ patient = Blueprint('patient', __name__,
                     template_folder='templates')
 
 
-@patient.route('/', methods=['GET'])
-def index():
+@patient.route('/p<int:patient_number>/', methods=['GET'])
+def index(patient_number):
+    if patient_number not in range(1, 12):
+        abort(404)
+
+    pname = 'p'+str(patient_number)
+
     return render_template('patient.html',
+                           pname=pname,
                            title='Patient page')
